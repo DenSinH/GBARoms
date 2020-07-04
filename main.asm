46ad6ba0a1f9d6639056e54fd448e1a8bc172213
Dennis <Dennis@DENNIS-PC>
2020-07-04 14:22:11 +0200

    changed to mode 3, added pixel setting

--- main.asm
+++ main.asm
@@ -17,59 +17,54 @@ macro set_half reg, value {
         orr reg, value and 0xff00
 }
 
-macro fill_pixel reg_x, reg_y {
-
-}
-
 header:
         include './lib/header.inc'; I just borrowed this from JSMolka, thanks for that!
 
 main:
         ; set DISPCNT to 0x0100: enable BG0
         set_word r0, DISPCNT
-        set_half r1, 0x0100
-        strh r1, [r0]
+        set_half r1, 0x0403
+        strh r1, [r0]        ; set BGMode to mode 3, display BG2
 
-        add r0, r0, #8       ; BG0CNT
-        mov r1, 0x04         ; set_half r1, 0x0040
-        strh r1, [r0]        ; set CharBaseBlock = 1, ScreenBaseBlock = 0, 4bpp mode
+        add r0, 0x02000000 ; start of VRAM
 
-        set_word r0, MEM_PALETTE or 0x2 ; second palette entry
-        set_half r1, FILL_COLOR         ; set palette entry to FILL_COLOR defined above
-        strh r1, [r0]
-
-        mov r2, 0                       ; tile counter
-        make_tiles:
-                set_word r0, MEM_VRAM or 0x4000 ; start of second CharBlock
-                mov r3, 0                       ; keeps track of top or bottom for us
-                add r0, r0, r2, lsl 5           ; tile r2 * 32
-
-                make_tile:
-                        mov r1, 0               ; tile data
-                        tst r0, 0x10            ; eq if top half, ne if bottom
-                        moveq r3, 0x01
-                        movne r3, 0x04          ; 1 if top half else 4
-
-                        tst r2, r3              ; left part of tile
-                        orrne r1, 0x11
-                        orrne r1, 0x1100
-
-                        mov r3, r3, lsl #0x01   ; 2 if top half else 8
-
-                        tst r2, r3              ; right part of tile
-                        orrne r1, 0x110000
-                        orrne r1, 0x11000000
-                        stmia r0!, { r1 }
-
-                        tst r0, 0x1f            ; check if we have reached the next tile
-                        bne make_tile           ; jump back if we have not yet reached the next tile
-                add r2, r2, 0x1
-                cmp r2, 0x10
-                bne make_tiles
-
-        and r0, 0xff000000 ; start of VRAM
-        mov r1, 0x01
-        strh r1, [r0]
+        mov r2, 0x40;
+        mov r0, 0x0
+        mov r1, 0x0
+        loop:
+                bl fill_pixel
+                add r0,  #1
+                add r1,  #1
+                subs r2, #1
+                bne loop
 
         wait:
                 b wait
+
+fill_pixel:
+    ; fill pixel at coord [r0, r1]
+    stmdb sp!, { r0, r1, r2, r3 }   ; save old values
+    add r0, r0, lsl #1       ; r0 *= 3
+    add r1, r1, lsl #1       ; r1 *= 3
+    mov r0, r0, lsl #1       ; r0 *= 2
+    mov r3, #480
+    mul r1, r1, r3           ; multiply y by screen width times 2
+    add r0, r1
+    add r0, MEM_VRAM         ; add VRAM start location to get start value of pixel
+    set_half r2, FILL_COLOR  ; color to fill with
+    mov r3, 0x3              ; vertical counter
+
+    fill_sliver:             ; fill sliver with pixel
+        strh r2, [r0]
+        add r0, #0x1
+        strh r2, [r0]
+        add r0, #0x1
+        strh r2, [r0]
+        add r0, #0x1
+        subs r3, 0x1
+        add r0, #480
+        sub r0, #3         ; add 128 (1 row horizontally), subtract 3
+        bne fill_sliver
+    ldmia sp!, { r0, r1, r2, r3 }
+    bx lr
+
