f1c8449a3cea1bfbffbaa404fb6ba0088a3b91a8
Dennis <dhilhorst2000@gmail.com>
2020-09-20 12:17:18 +0200

    added EEPROM tests

--- eeprom-test/tests.asm
+++ eeprom-test/tests.asm
@@ -3,7 +3,7 @@
 ;
 ;       We are going to use 8kb EEPROM
 ;
-;       r0, r1, r2, r3: GP
+;       r0, r1, r2, r3, r4: GP
 ;       r5: ??? (command)
 ;       r6: ??? (command)
 ;       r7: ??? (command)
@@ -16,7 +16,11 @@
 macro send_read_address address {
         local store_loop
 
+      if address in <r0, r1, r2, r3, r4, r5, r6, r7, r8, r9, r10, r11, r12, r13, r14, r15, sp, lr, pc>
+        orr r5, address, \#0xc000
+      else
         set_half r5, \#((address) or (0xc000)) ; set (to address << 2) | c000 (read mode bits)
+      end if
         mov r6, MEM_EWRAM
         add r6, \#30     ; 16 bits to be stored, 2 bytes per bit, last bit is 0
 
@@ -44,6 +48,8 @@ macro read_data {
         local load_loop
 
         ; read data and push to r0, r1 (8 bytes (stored as 2 words), 64 bits)
+        ; r2 holds whether we resolve mirroring (1) or not (0xff)
+
         str r9, [r11]         ; SAD
         mov r5, MEM_EWRAM
         str r5, [r11, \#4]    ; DAD
@@ -59,6 +65,7 @@ macro read_data {
 
         load_loop:
                 ldrh r7, [r5], \#2      ; load halfword (bit)
+                and r7, r2              ; resolve mirroring (or not)
                 orr r8, r7, r8, lsl \#1 ; buffer bit
                 sub r6, \#1
                 tst r6, \#31
@@ -73,9 +80,14 @@ macro write_data address {
         ; write data in r0, r1 to address
         local addr_store_loop
         local data_store_loop
+        local write_complete_wait
 
         ; store address + mode bits
+      if address in <r0, r1, r2, r3, r4, r5, r6, r7, r8, r9, r10, r11, r12, r13, r14, r15, sp, lr, pc>
+        orr r5, address, \#0x8000
+      else
         set_half r5, \#((address) or (0x8000)) ; set (to address << 2) | 8000 (write mode bits)
+      end if
         mov r6, MEM_EWRAM
         add r6, \#30     ; 16 bits to be stored, 2 bytes per bit, last bit is 0
 
@@ -88,7 +100,7 @@ macro write_data address {
         strh r5, [r6]           ; last bit wasn't stored yet
 
         mov r7, #64             ; bit counter
-        add r6, \#32 + 128      ; store data past address (counted in half words!)
+        add r6, \#30 + 128      ; store data past address (counted in half words!)
         stmfd sp!, \{ r0, r1 \} ; store data to stack
         ldmfd sp!, \{ r5 \}
 
@@ -110,10 +122,17 @@ macro write_data address {
         strh r6, [r11, \#8]   ; set CNT_L
         strh r10, [r11, \#10] ; set CNT_H
 
-        ; wait for potential DMA delay
+        ; DMA start delay
         nop
         nop
         nop
+
+        ; keep reading from the chip until bit 0 becomes 1
+        write_complete_wait:
+                ldrh r5, [r9]
+                add r6, \#1
+                tst r5, \#1
+                beq write_complete_wait
 }
 
 test_setup:
@@ -122,10 +141,11 @@ test_setup:
         set_word r11, 0x040000D4
 
 test_0:
-        ; read from unused area (initialized to 0xff)
+        ; read from unused area (initialized to 0xff) with resolved mirroring
         mov r12, #0
 
-        send_read_address 0x3fff  ; read top 8 bytes
+        mov r2, #1
+        send_read_address 0x3ff  ; read top 8 bytes
         read_data
 
         cmp r0, #0xffffffff
@@ -133,23 +153,141 @@ test_0:
         bne fail_test
 
 test_1:
-        ; basic read/write test
+        ; basic read/write test (with mirroring resolved)
         mov r12, #1
 
-        set_word r0, #0x69696969
-        set_word r1, #0x04200420
+        set_word r0, 0x69696969
+        set_word r1, 0x04200420
 
-        mov r2, r0
-        mov r3, r1
+        mov r3, r0
+        mov r4, r1
 
         write_data 0x0000         ; write to first entry
+
+        ; make sure you can't pass this test by doing nothing
+        mov r0, #0
+        mov r1, #0
+
+        ; resolve mirroring
+        mov r2, #1
 
         send_read_address 0x0000  ; read it back
         read_data
 
-        cmp r0, r2
-        cmpeq r1, r3
+        cmp r0, r3
+        cmpeq r1, r4
         bne fail_test
+
+test_2:
+        ; write/read from consecutive 8 byte areas
+        ; addressing happens in multiples of 8 bytes (so 1 is 8 bytes further than 0)
+        ; this means that writing different values to 1 than we just did to 0 should yield us the
+        ; right values back from 0 _and_ 1
+        mov r12, #2
+
+        set_word r0, 0x01234567
+        set_word r1, 0x89abcdef
+
+        mov r3, r0
+        mov r4, r1
+
+        write_data 0x0001         ; write to second entry
+
+        ; resolve mirroring
+        mov r2, #1
+        send_read_address 0x0001
+        read_data
+
+        cmp r0, r3
+        cmpeq r1, r4
+        bne fail_test
+
+        ; read first entry again
+        mov r2, #1
+        send_read_address 0x0000
+        read_data
+
+        ; same data as test 0
+        set_word r3, 0x69696969
+        set_word r4, 0x04200420
+
+        cmp r0, r3
+        cmpeq r1, r4
+        bne fail_test
+
+test_3:
+        ; test weird backup reads
+        ; write 0x00ff00ff to 0002, mirroring should return this as 0xffffffff
+        mov r12, #3
+
+        set_word r0, #0x00ff00ff
+        set_word r1, #0x00ff00ff
+
+        write_data 0x0002
+
+        ; anti-cheese
+        mov r0, #0
+        mov r1, #0
+
+        mov r2, #0xffffffff  ; DON'T resolve mirroring
+        send_read_address 0x0002
+        read_data
+
+        cmp r0, #0xffffffff
+        cmpeq r1, #0xffffffff
+        bne fail_test
+
+test_4:
+        ; test mirroring (read from 0x0D000000) because this is a small ROM
+        mov r12, #4
+
+        set_word r0, #0xdeadbeef
+        set_word r1, #0xfadecafe
+
+        mov r3, r0
+        mov r4, r1
+
+        mov r9, #0x0d000000
+
+        write_data 0x0003
+
+        mov r2, #1      ; resolve mirroring
+
+        send_read_address 0x0003
+        read_data
+
+        cmp r0, r3
+        cmpeq r1, r4
+        bne fail_test
+
+test_stress:
+        ; write to every address (except the last one to save initial state for re-runs)
+        ; read immediately after
+        set_word r12, #0xdeaddead
+
+        mov r2, #0x01   ; resolve read mirroring
+        mov r3, #0      ; address counter
+        set_half r4, #0x3ff ; max address
+
+        stress_test_loop:
+                mov r0, r3
+                mov r1, r3, lsl #16
+
+                write_data r3
+
+                mov r0, #0
+                mov r1, #0
+
+                send_read_address r3
+                read_data
+
+                cmp r0, r3
+                cmpeq r1, r3, lsl #16
+                bne fail_test
+
+                add r3, #1
+                cmp r3, r4      ; do not write to max address
+                blt stress_test_loop
 
         b pass_test
 
